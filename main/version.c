1a55107d4dd21cb030919d23a4a5f059bdae03c1
Anton Prins <a.prins@d-r.nl>
2010-06-15 10:54:10 +0200

    Changed major application firmware version

--- main/version.c
+++ main/version.c
@@ -5,7 +5,7 @@
 //
 // (C) COPYRIGHT 2004 TC Applied Technologies Ltd. (ALL RIGHTS RESERVED)
 //
-// Unauthorized use, duplication or distribution of this software is  
+// Unauthorized use, duplication or distribution of this software is
 // strictly prohibited by law.
 //
 // The entire notice above must be reproduced on all authorised copies and
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "targetVendorDefs.h"
-#include "tcat_dice_version.h" 
+#include "tcat_dice_version.h"
 
 
 
@@ -34,12 +34,12 @@ char _diceApp_time[] CYGBLD_ATTRIB_WEAK = __TIME__;
 //Sub next 4 bits (0..15)
 //Build last 16 bits (0..65535)
 
-#define APP_MAJ	0
+#define APP_MAJ 1
 #define APP_MIN 0
 #define APP_SUB 0
 #define APP_BUILD 0
 
-#define APP_VER32	((APP_MAJ<<24)|(APP_MIN<<20)|(APP_SUB<<16)|(APP_BUILD))
+#define APP_VER32   ((APP_MAJ<<24)|(APP_MIN<<20)|(APP_SUB<<16)|(APP_BUILD))
 
 #ifdef _USE_OLD_VERSION_INFO
 
@@ -47,10 +47,10 @@ char _diceApp_time[] CYGBLD_ATTRIB_WEAK = __TIME__;
 // DEPRECIATED AFTER SDK BUILD 108
 
 // This struct embeds the version info into the compiled binary, allowing host applications
-//	to find the values in the image. Host utilites can search for the GUID below, and then use 
-//  the adjacent info to verify versions, target model info, etc. before updating target device 
-//	application code. If your product will allow firmware updates, then make sure that the vendor 
-//	ID and product ID strings match your values in targetVendorDefs.h
+//  to find the values in the image. Host utilites can search for the GUID below, and then use
+//  the adjacent info to verify versions, target model info, etc. before updating target device
+//  application code. If your product will allow firmware updates, then make sure that the vendor
+//  ID and product ID strings match your values in targetVendorDefs.h
 // ** Also, never change the MagicNumb (GUID) below if you wish to use utilities provided with the development tools.
 
 //
@@ -59,61 +59,61 @@ char _diceApp_time[] CYGBLD_ATTRIB_WEAK = __TIME__;
 
 typedef struct
 {
-	char MagicNum[36];		// initialized below, do not change this number
-	char Major[5];			// 4 digits followed by a space
-	char Minor[5];			// 4 digits followed by a space
-	char VendorID[7];		// 6 digit 24-bit OUI (as defined in targetVendorDefs.h) followed by a space
-	char VProductID[4];		// 3 digit (10 bits) Product ID (as defined in targetVendorDefs.h) followed by a space
-	char VMajor[5];			// 4 digits followed by a space
-	char VMinor[5];			// 4 digits followed by a space
+    char MagicNum[36];      // initialized below, do not change this number
+    char Major[5];          // 4 digits followed by a space
+    char Minor[5];          // 4 digits followed by a space
+    char VendorID[7];       // 6 digit 24-bit OUI (as defined in targetVendorDefs.h) followed by a space
+    char VProductID[4];     // 3 digit (10 bits) Product ID (as defined in targetVendorDefs.h) followed by a space
+    char VMajor[5];         // 4 digits followed by a space
+    char VMinor[5];         // 4 digits followed by a space
 
 } VERSION;
 
-static VERSION rev = { kTCAT_DICE_VERSION_MAGIC_STRING, 
-	kTCAT_DICE_VERSION_MAJOR_STRING, 
-	kTCAT_DICE_VERSION_MINOR_STRING,
-	"", 
-	"", 
-	"0000",
-	"0000"
+static VERSION rev = { kTCAT_DICE_VERSION_MAGIC_STRING,
+    kTCAT_DICE_VERSION_MAJOR_STRING,
+    kTCAT_DICE_VERSION_MINOR_STRING,
+    "",
+    "",
+    "0000",
+    "0000"
 };
 
 char * diceApp_vendor_id(void)
 {
-	return rev.VendorID;
+    return rev.VendorID;
 }
 
 int diceApp_vendor_major_rev(void)
 {
-	return atoi(rev.VMajor);
+    return atoi(rev.VMajor);
 }
 
 int diceApp_vendor_minor_rev(void)
 {
-	return atoi(rev.VMinor);
+    return atoi(rev.VMinor);
 }
 
 #else //Suggested version info structure
 
 typedef struct
 {
-	char MagicNum[36];		// initialized below, do not change this number
-	uint32	uiBaseSDKVersion;				//The full version/revision of the SDK this build was based on
-	uint32	uiApplicationVersion;		//The full version/revision of the Application
-	uint32	uiVendorID;							//The Vendor ID
-	uint32	uiProductID;						//The product ID
-	char		BuildTime[64];					//Build time
-	char		BuildDate[64];					//Build date
+    char MagicNum[36];      // initialized below, do not change this number
+    uint32  uiBaseSDKVersion;               //The full version/revision of the SDK this build was based on
+    uint32  uiApplicationVersion;       //The full version/revision of the Application
+    uint32  uiVendorID;                         //The Vendor ID
+    uint32  uiProductID;                        //The product ID
+    char        BuildTime[64];                  //Build time
+    char        BuildDate[64];                  //Build date
 } VERSION;
 
-static VERSION rev = { 
-	kTCAT_DICE_VERSION_NEW_MAGIC_STRING, 
-	kTCAT_DICE_VERSION32,						//The full version/revision of the SDK this build was based on
-	APP_VER32,											//The full version/revision of the Application
-	THIS_VENDOR_ID,									//The Vendor ID
-	THIS_PRODUCT_ID,								//The product ID
-	__TIME__,												//Build time
-	__DATE__												//Build date
+static VERSION rev = {
+    kTCAT_DICE_VERSION_NEW_MAGIC_STRING,
+    kTCAT_DICE_VERSION32,                       //The full version/revision of the SDK this build was based on
+    APP_VER32,                                          //The full version/revision of the Application
+    THIS_VENDOR_ID,                                 //The Vendor ID
+    THIS_PRODUCT_ID,                                //The product ID
+    __TIME__,                                               //Build time
+    __DATE__                                                //Build date
 };
 
 //These functions are maintained for backward compatibility
@@ -123,88 +123,88 @@ static char vendorIdCache[7];
 //Vendor ID as string
 char * diceApp_vendor_id(void)
 {
-	sprintf(vendorIdCache,"%06X",rev.uiVendorID&0xFFFFFF);
-	return vendorIdCache;
+    sprintf(vendorIdCache,"%06X",rev.uiVendorID&0xFFFFFF);
+    return vendorIdCache;
 }
 
 uint32 diceApp_vendor_major_rev(void)
 {
-	return APP_MAJ;
+    return APP_MAJ;
 }
 
 uint32 diceApp_vendor_minor_rev(void)
 {
-	return APP_MIN;
+    return APP_MIN;
 }
 
 // New functions to retrieve the full 32 bit vendor application version
 
 uint32 diceApp_vendor_sub_rev(void)
 {
-	return APP_SUB;
+    return APP_SUB;
 }
 
 uint32 diceApp_vendor_build_rev(void)
 {
-	return APP_BUILD;
+    return APP_BUILD;
 }
 
 uint32 diceApp_vendor_full_rev(void)
 {
-	return APP_VER32;
+    return APP_VER32;
 }
 
 #endif
 
 
 
-char * diceApp_build_time(void) 
+char * diceApp_build_time(void)
 {
-	return _diceApp_build_time;
+    return _diceApp_build_time;
 }
 
-char * diceApp_date(void) 
+char * diceApp_date(void)
 {
-	return _diceApp_date;
+    return _diceApp_date;
 }
 
-char * diceApp_time(void) 
+char * diceApp_time(void)
 {
-	return _diceApp_time;
+    return _diceApp_time;
 }
 
 
 int diceApp_product_id(void)
 {
-	return THIS_PRODUCT_ID;
+    return THIS_PRODUCT_ID;
 }
 
 uint32 diceApp_vendor_id_num(void)
 {
-	return THIS_VENDOR_ID;
+    return THIS_VENDOR_ID;
 }
 
-uint32	diceApp_major_rev(void) 
+uint32  diceApp_major_rev(void)
 {
-	return kTCAT_DICE_VERSION_MAJOR;
+    return kTCAT_DICE_VERSION_MAJOR;
 }
 
-uint32	diceApp_minor_rev(void)
+uint32  diceApp_minor_rev(void)
 {
-	return kTCAT_DICE_VERSION_MINOR;
+    return kTCAT_DICE_VERSION_MINOR;
 }
 
-uint32	diceApp_sub_rev(void) 
+uint32  diceApp_sub_rev(void)
 {
-	return kTCAT_DICE_VERSION_SUB;
+    return kTCAT_DICE_VERSION_SUB;
 }
 
-uint32	diceApp_build_rev(void) 
+uint32  diceApp_build_rev(void)
 {
-	return kTCAT_DICE_VERSION_BUILD;
+    return kTCAT_DICE_VERSION_BUILD;
 }
 
-uint32	diceApp_full_rev(void) 
+uint32  diceApp_full_rev(void)
 {
-	return kTCAT_DICE_VERSION32;
+    return kTCAT_DICE_VERSION32;
 }
